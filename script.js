76ca416962038b7547e38f2fb75fbf6d7ba790cc
Saurabh Verma <121659031+Saurabhv1584@users.noreply.github.com>
2023-01-11 22:46:13 -0800

    Update script.js

--- script.js
+++ script.js
@@ -26,3 +26,49 @@ function scrollVertically(targetSection){
     }
     window.scrollBy(0, 50);
 }
+
+
+// Animation on Skill Bar
+
+var progressBars = document.querySelectorAll(".skill-progress > div");
+var skillsContainer = document.getElementById('skills-container');
+var animationDone = false;
+window.addEventListener("scroll", checkScroll);
+
+
+function initialiseBars(){
+    for (var bar of progressBars) {
+        bar.style.width = 0 + '%' ;
+    }
+}
+
+initialiseBars();
+
+function fillBars(){
+    function fillBars() {
+
+        for (let bar of progressBars) {
+            let currentWidth = 0;
+            let interval = setInterval(function () {
+                let targetWidth = bar.getAttribute('data-bar-width');
+                if (currentWidth >= targetWidth) {
+                    clearInterval(interval);
+                    return;
+                }
+                currentWidth++;
+                bar.style.width = currentWidth + '%';
+            }, 5);
+        }
+    }
+
+function checkScroll(){
+    // check whether skill container is visible
+    var coordinates = skillsContainer.getBoundingClientRect();
+    if(!animationDone && coordinates.top < window.innerHeight){
+        animationDone = true;
+         fillBars();
+    }else if(coordinates.top > window.innerHeight){
+        animationDone = false;
+        initialiseBars();
+    }
+}
